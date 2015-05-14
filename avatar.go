fbbc4a3482603b9bb43a5ffe7372f5021436be67
nemowen <nemo.emails@gmail.com>
2015-05-14 11:18:09 +0800

    The authentication service and avatar's implementation

---
+++ avatar.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+)
+
+// 当Avatar不能提供一个URL的时候
+var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL.")
+
+type Avatar interface {
+	GetAvatarURL(c *client) (string, error)
+}
+
+type AuthAvatar struct{}
+
+var UseAuthAvatar AuthAvatar
+
+func (_ AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	if url, ok := c.userData["avatar_url"]; ok {
+		if urlStr, ok := url.(string); ok {
+			return urlStr, nil
+		}
+	}
+	return "", ErrNoAvatarURL
+}
